ee61a1794f21ce994ec7cc9e3df6eecef5c3d86e
Gustavo Monarin dos Santos <gustavomonarin11@gmail.com>
2023-08-18 19:32:12 -0300

    corrigindo os bug dos validadores

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 const input = document.getElementById("numero");
 
-input.addEventListener("blur", validarCampo());
+//Validar se contem 8 digitos
+input.addEventListener("input", function () {
+  const numeroBin = input.value;
+  if (numeroBin.length > 8) {
+    input.value = numeroBin.slice(0, 8);
+  }
+});
 
-function validarCampo() {
+// Validar se contem apenas 0 e 1 dando uma mensagem de erro
+input.addEventListener("blur", function () {
   const numeroBin = input.value;
   let regex = /^[0-1]+$/;
 
+  const mensagemErro = input.nextElementSibling;
+  if (mensagemErro.classList.contains("mensagem-erro")) {
+    mensagemErro.remove();
+  }
+
   if (!regex.test(numeroBin)) {
     const mensagemErro = document.createElement("p");
+    mensagemErro.classList.add("mensagem-erro");
     mensagemErro.innerHTML =
       "Não é um número binário válido, digite novamente!";
     input.insertAdjacentElement("afterend", mensagemErro);
   }
-}
+});
