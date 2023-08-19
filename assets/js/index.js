16b563f8c8bae95b00c75648aba5c24d8682e61c
Gustavo Monarin dos Santos <gustavomonarin11@gmail.com>
2023-08-19 00:54:09 -0300

    implementado a funcao para converter o numero bin em decimal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,34 @@
 const input = document.getElementById("numero");
+const btn = document.getElementById("calcular");
 
 //Validar se contem 8 digitos
+/*
 input.addEventListener("input", function () {
   const numeroBin = input.value;
   if (numeroBin.length > 8) {
     input.value = numeroBin.slice(0, 8);
   }
+});*/
+
+// Fazer as validações necessarias
+input.addEventListener("blur", validarCampo);
+
+// Transformar numero binario em decimal
+
+btn.addEventListener("click", function () {
+  const numeroBin = input.value;
+  let decimal = 0;
+  if (validarCampo()) {
+    for (let index = numeroBin.length - 1; index >= 0; index--) {
+      decimal +=
+        parseInt(numeroBin(index)) * Math.pow(2, bin.length - 1 - index);
+    }
+    console.log(decimal);
+  }
 });
 
-// Validar se contem apenas 0 e 1 dando uma mensagem de erro
-input.addEventListener("blur", function () {
+//validar os campos
+function validarCampo() {
   const numeroBin = input.value;
   let regex = /^[0-1]+$/;
 
@@ -18,11 +37,18 @@ input.addEventListener("blur", function () {
     mensagemErro.remove();
   }
 
-  if (!regex.test(numeroBin)) {
+  if (numeroBin === "") {
+    const mensagemErro = document.createElement("p");
+    mensagemErro.classList.add("mensagem-erro");
+    mensagemErro.innerHTML = "Campo em branco, digite número";
+    input.insertAdjacentElement("afterend", mensagemErro);
+  } else if (!regex.test(numeroBin)) {
     const mensagemErro = document.createElement("p");
     mensagemErro.classList.add("mensagem-erro");
     mensagemErro.innerHTML =
       "Não é um número binário válido, digite novamente!";
     input.insertAdjacentElement("afterend", mensagemErro);
+  } else {
+    return true;
   }
-});
+}
