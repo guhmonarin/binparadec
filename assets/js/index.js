89cfed99491e3b898d308d40ff6dea25e99b8e9f
Gustavo Monarin dos Santos <gustavomonarin11@gmail.com>
2023-08-19 16:39:01 -0300

    adicionando a versao completa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,6 @@
 const input = document.getElementById("numero");
 const btn = document.getElementById("calcular");
-
-//Validar se contem 8 digitos
-/*
-input.addEventListener("input", function () {
-  const numeroBin = input.value;
-  if (numeroBin.length > 8) {
-    input.value = numeroBin.slice(0, 8);
-  }
-});*/
+const subTitulo = document.querySelector(".sub-titulo");
 
 // Fazer as validações necessarias
 input.addEventListener("blur", validarCampo);
@@ -16,14 +8,20 @@ input.addEventListener("blur", validarCampo);
 // Transformar numero binario em decimal
 
 btn.addEventListener("click", function () {
-  const numeroBin = input.value;
-  let decimal = 0;
-  if (validarCampo()) {
-    for (let index = numeroBin.length - 1; index >= 0; index--) {
-      decimal +=
-        parseInt(numeroBin(index)) * Math.pow(2, bin.length - 1 - index);
+  if (btn.innerHTML == "Voltar") {
+    crearElementos();
+  } else {
+    const numeroBin = input.value;
+    let decimal = 0;
+    if (validarCampo()) {
+      for (let index = numeroBin.length - 1; index >= 0; index--) {
+        decimal +=
+          parseInt(numeroBin[index]) *
+          Math.pow(2, numeroBin.length - 1 - index);
+      }
+
+      resultado(decimal);
     }
-    console.log(decimal);
   }
 });
 
@@ -45,10 +43,31 @@ function validarCampo() {
   } else if (!regex.test(numeroBin)) {
     const mensagemErro = document.createElement("p");
     mensagemErro.classList.add("mensagem-erro");
-    mensagemErro.innerHTML =
-      "Não é um número binário válido, digite novamente!";
+    mensagemErro.innerHTML = "Número inválido, digite novamente";
     input.insertAdjacentElement("afterend", mensagemErro);
   } else {
     return true;
   }
+}
+
+// resultado
+function resultado(decimal) {
+  input.style.display = "none";
+  btn.innerHTML = "Voltar";
+
+  subTitulo.innerHTML = "O número decimal é:";
+  const resultado = document.createElement("p");
+  resultado.classList = "resultado";
+  resultado.innerHTML = decimal;
+  subTitulo.insertAdjacentElement("afterend", resultado);
+}
+
+//crear os elementos
+function crearElementos() {
+  subTitulo.innerHTML = "Digite um número binário:";
+  input.style.display = "block";
+  input.value = "";
+  const resultado = document.querySelector(".resultado");
+  resultado.remove();
+  btn.innerHTML = "Calcular";
 }
